4c4357b8907a4a834c6e7367af880a62473344e1
Sona Khachatryan <sona.khachatryannn@gmail.com>
2024-05-20 19:03:15 +0400

    features all screen sizes

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <main>
          <Header/>
          <Hero/>
-         {/*<Features/>*/}
+         <Features/>
       </main>
    );
 }
--- components/Features/Features.jsx
+++ components/Features/Features.jsx
@@ -3,8 +3,41 @@ import React from 'react';
 function Features(props) {
    return (<>
       <section className='features container'>
-         <p>What will you get?</p>
-         <div className='features__container'>
+         <p className='features__title'>What will you get?</p>
+         <div className='features__icon-container'>
+            <img className='wave' src='wave.svg'/>
+            <div className='features__icon-text-group'>
+               <div className='features__icon'>
+                  <img className='features__rectangle' src='Rectangle.svg'/>
+                  <img className='features__vector chat' src='chat.svg'/>
+               </div>
+               <p className='features__icon-description'>Chat with players</p>
+            </div>
+
+            <div className='features__icon-text-group'>
+               <div className='features__icon'>
+                  <img className='features__rectangle' src='Rectangle.svg'/>
+                  <img className='features__vector pencil' style={{zIndex: 30}} src='pencil.svg'/>
+                  <img className='features__vector paper' src='Vector.svg'/>
+               </div>
+               <p className='features__icon-description'>Create a game</p>
+            </div>
+
+            <div className='features__icon-text-group'>
+               <div className='features__icon'>
+                  <img className='features__rectangle' src='Rectangle.svg'/>
+                  <img className='features__vector stadium' src='stadium.svg'/>
+               </div>
+               <p className='features__icon-description'>Find nearby stadiums</p>
+            </div>
+
+            <div className='features__icon-text-group'>
+               <div className='features__icon'>
+                  <img className='features__rectangle' src='Rectangle.svg'/>
+                  <img className='features__vector invitation' src='invitation.svg'/>
+               </div>
+               <p className='features__icon-description'>Receive an invitation</p>
+            </div>
 
          </div>
       </section>
