fe582bd7213d7c31ae1242868dc49ffea4bae7c5
Sona Khachatryan <sona.khachatryannn@gmail.com>
2024-05-23 19:19:17 +0400

    contact section all screens

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -14,54 +14,86 @@ function Contact(props) {
    const handleMessageInputChange = () => {};
 
    return (
-      <div className='contact-section container'>
+      <div className='container'>
+         <div className='contact-section-wrapper'>
+            <div className='contact-section'>
 
-         <div className='contact-form'>
-            <p className='contact-form__title'>
-               Contact Us
-            </p>
-            <input className='contact-form__name-input' placeholder='Enter your Name'/>
-            <input className='contact-form__email-input' placeholder='Enter a valid email address'/>
-            <input className='contact-form__message-input' placeholder='Enter your message'/>
-            <button className='contact-form__submit-btn'>Submit</button>
-         </div>
+               <div className='contact-info-wrapper'>
+                  <div className='contact-info'>
+                     <div className='contact-info__section-title'>
+                        Contact information
+                     </div>
+                     <div className="contact-info__section-wrapper">
+                        <div className='contact-info__section'>
+                           <img className='contact-info__phone-icon' src='phone-vector.svg' alt='phone'/>
+                           <div className='contact-info__text'>
+                              <p className='contact-info__title'>
+                                 Give Us a Call
+                              </p>
+                              <p className='contact-info__sub'>
+                                 + (374) 99-99-99
+                              </p>
+                           </div>
+                        </div>
+                     </div>
 
-         <div className='contact-info'>
-            <div className='contact-info__section'>
-               <img className='contact-info__icon' src='phone-vector.svg' alt='phone'/>
-               <p className='contact-info__title'>
-                  Give Us a Call
-               </p>
-               <p className='contact-info__sub'>
-                  + (374) 99-99-99
-               </p>
+                     <div className='contact-info__line1'></div>
+
+                     <div className="contact-info__section-wrapper message-wrapper">
+                        <div className='contact-info__section'>
+                           <img className='contact-info__message-icon' src='message.svg' alt='message'/>
+                           <div className='contact-info__text'>
+                              <p className='contact-info__title'>
+                                 Send Us a Message
+                              </p>
+                              <p className='contact-info__sub'>
+                                 contanct@ballhola.app
+                              </p>
+                           </div>
+                        </div>
+                     </div>
+
+                     <div className='contact-info__line2'></div>
+
+                     <div className="contact-info__section-wrapper">
+                        <div className='contact-info__section socials-section'>
+                           <p className='contact-info__title'>Social Media</p>
+                           <div className='contact-info__socials'>
+                              <img className='contact-info__socials-icon' src='instagram.svg' alt='instagram'/>
+                              <img className='contact-info__socials-icon' src='facebook.svg' alt='facebook'/>
+                              <img className='contact-info__socials-icon' src='linkedin.svg' alt='linkedin'/>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+
+               <div className='contact-info__line'></div>
+
+               <div className='contact-form-wrapper'>
+                  <div className='contact-form'>
+                     <p className='contact-form__title'>
+                        Contact Us
+                     </p>
+                     <input className='contact-form__input' placeholder='Enter your name'/>
+                     <input className='contact-form__input' placeholder='Enter a valid email address'/>
+                     <textarea className='contact-form__input contact-form__message-input'
+                        placeholder='Enter your message'/>
+                     <button className='contact-form__submit-btn'>Submit</button>
+                  </div>
+
+               </div>
+               <div className='contact-info__toast'></div>
+
+               <div className='contact-section__animation'>
+                  <img/>
+                  <img/>
+                  <img/>
+                  <img/>
+                  <img/>
+                  <img/>
+               </div>
             </div>
-            <div className='contact-info__section'>
-               <img className='contact-info__icon' src='message.svg' alt='message'/>
-               <p className='contact-info__title'>
-                  Send Us a Message
-               </p>
-               <p className='contact-info__sub'>
-                  contanct@ballhola.app
-               </p>
-            </div>
-            <div className='contact-info__section'>
-               <p className='contact-info__title'>Social Media</p>
-               <img className='contact-info__socials' src='instagram.svg' alt='instagram'/>
-               <img className='contact-info__socials' src='facebook.svg' alt='facebook'/>
-               <img className='contact-info__socials' src='linkedin.svg' alt='linkedin'/>
-            </div>
-         </div>
-
-         <div className='contact-info__toast'></div>
-
-         <div className='contact-section__animation'>
-            <img/>
-            <img/>
-            <img/>
-            <img/>
-            <img/>
-            <img/>
          </div>
       </div>
    );
