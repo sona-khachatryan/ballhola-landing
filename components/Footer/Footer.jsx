89401e7020d07571323a8fe6927ba42801cd8e88
Sona Khachatryan <sona.khachatryannn@gmail.com>
2024-05-27 15:34:26 +0400

    footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -4,7 +4,33 @@ import React from 'react';
 
 function Footer(props) {
    return (
-      <div></div>
+      <div>
+         <footer className='footer container'>
+            <nav>
+               <ul className='footer__menu'>
+                  <li>
+                     <img className='footer__logo' src='Logo.svg' alt='logo'/>
+                  </li>
+                  <li>
+                     <a className='footer__link' href='#features'>Features</a>
+                  </li>
+                  <li>
+                     <a className='footer__link' href='#demo'>Demonstration</a>
+                  </li>
+                  <li>
+                     <a className='footer__link' href='#reviews'>Reviews</a>
+                  </li>
+                  <li>
+                     <a className='footer__link' href='#download'>Download</a>
+                  </li>
+                  <li>
+                     <a className='footer__link' href='#contact'>Contact Us</a>
+                  </li>
+               </ul>
+            </nav>
+            <p className='footer__text'>© 2024 BallHola All rights reserved</p>
+         </footer>
+      </div>
    );
 }
 
